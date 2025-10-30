5474bec84fe5a4398cd43b13bdd7245bef3f30cf
MacrocConsultants <Macroc.consultants@zohomail.in>
2025-10-31 01:00:11 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        macrocGreen: '#0f8a5f',
+        macrocGold: '#d4af37'
+      }
+    },
+  },
+  plugins: [],
+}
